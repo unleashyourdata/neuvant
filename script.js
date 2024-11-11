efae44d52e6575f27764c9f429ecd5cb329a754e
unleashyourdata <91387578+unleashyourdata@users.noreply.github.com>
2024-11-11 14:31:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuItems = document.querySelectorAll('.menu-item');
 
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      const submenu = item.querySelector('.submenu');
+      if (submenu) {
+        submenu.classList.toggle('open');
+      }
+    });
+  });
+});
